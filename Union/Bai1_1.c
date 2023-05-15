049bffcd3c21831abb4598cbf0a4922c72bece21
NguyenDangTrieu <nguyendangtrieu01042001@gmail.com>
2023-05-15 18:08:08 +0700

    Modify Union/Bai1_1.c

--- Union/Bai1_1.c
+++ Union/Bai1_1.c
@@ -1,20 +1,30 @@
 #include<stdio.h>
 
+#pragma pack(1)
+
+/* Khi bạn sử dụng #pragma pack(1), bạn đang yêu cầu trình biên dịch không thêm bất kỳ byte trống (padding) nào 
+vào giữa các thành phần của cấu trúc. Thay vào đó, các thành phần sẽ được căn chỉnh liền kề nhau, với kích thước
+tối thiểu cần thiết.
+   Việc sử dụng #pragma pack(1) có thể giúp tối ưu kích thước bộ nhớ và đảm bảo tính nhất quán trong việc truy cập
+và trao đổi dữ liệu.
+   Tuy nhiên, cần lưu ý rằng việc loại bỏ padding có thể ảnh hưởng đến hiệu suất của chương trình, do truy cập
+vào các thành phần của cấu trúc có thể chậm hơn nếu các thành phần không được căn chỉnh tối ưu cho kiến trúc của
+máy tính. Điều này đặc biệt quan trọng đối với các kiến trúc bộ nhớ yêu cầu dữ liệu phải căn chỉnh đúng.*/
 struct store{
-    double price;           
+    double price;            // 8 bytes
     union {
         struct 
         {
-            char *title;    
-            char *author;   
-            int num_pages;  
+            char *title;    // 4 bytes
+            char *author;   // 4 bytes
+            int num_pages;  // 4 bytes
         } book;
 
         struct 
         {
-            int color;      
-            int size;       
-            char *design;   
+            int color;      // 4 bytes
+            int size;       // 4 bytes
+            char *design;   // 4 bytes
         } shirt;
     } items;
 };
@@ -27,7 +37,7 @@ int main()
     s.items.book.num_pages = 350;
     s.price = 10;
     
-    printf("\nSize of struct store: %d bytes\n",sizeof(struct store)); // Output: Size of struct store: 24 bytes
+    printf("\nSize of struct store: %d bytes\n",sizeof(struct store)); // Output: Size of struct store: 20 bytes
 
     //Hien thi ra man hinh
     printf("\n-Title: %s\n",s.items.book.title);
